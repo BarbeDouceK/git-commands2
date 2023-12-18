2b6a9144cf13517c1fb9997bb3988653fe6ca8f9
BarbeDouce <baptiste.louerat44@gmail.com>
2023-12-18 12:47:28 +0100

    fix : Corrige un bogue

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,6 +1,6 @@
 public class AnotherClass {
     public static void main(String[] args) {
 
-        System.out.println("AnotherClass.main  ");
+        System.out.println("AnotherClass.main");
     }
 }
